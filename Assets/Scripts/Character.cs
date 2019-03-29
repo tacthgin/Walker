3d9e0341f7418a9b4e466a5ef2339c97b8ef041a
tacthgin <821910196@qq.com>
2019-03-29 11:15:18 +0800

    first commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    protected float speed;
+
+    protected Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    protected void Move()
+    {
+        transform.Translate(speed * direction * Time.deltaTime);
+    }
+}
