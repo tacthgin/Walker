0992e6fe46c7a5ef62796e510d2d02b4e9735797
tacthgin <821910196@qq.com>
2019-06-11 16:19:45 +0800

    tilemap
    地图使用tilemap

---
+++ Assets/Scripts/TileScripts/TreeTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TreeTile : Tile
+{
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        if (go != null)
+        {
+            go.GetComponent<SpriteRenderer>().sortingOrder = -position.y * 2;
+        }
+        
+        return base.StartUp(position, tilemap, go);
+    }
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Tiles/TreeTile")]
+    public static void CreateWaterTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Treetile", "New Treetile", "asset", "Save Treetile", "Assets");
+        if (path == "")
+        {
+            return;
+        }
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<TreeTile>(), path);
+    }
+#endif
+}
