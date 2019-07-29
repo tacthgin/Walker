e1944e6f97f949e62d057117665cfea88198d58e
tacthgin <821910196@qq.com>
2019-07-29 11:36:51 +0800

    tooltip 添加物品品质颜色

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Quality { Common, Uncommon, Rare, Epic }
+
 public abstract class Item : ScriptableObject, IMoveable, IDescribable
 {
     [SerializeField]
@@ -19,9 +21,30 @@ public abstract class Item : ScriptableObject, IMoveable, IDescribable
     [SerializeField]
     private string title;
 
+    [SerializeField]
+    private Quality quality;
+
     public string GetDescription()
     {
-        return title;
+        string color = string.Empty;
+
+        switch (quality)
+        {
+            case Quality.Common:
+                color = "#d6d6d6";
+                break;
+            case Quality.Uncommon:
+                color = "#00ff00ff";
+                break;
+            case Quality.Rare:
+                color = "#0000ffff";
+                break;
+            case Quality.Epic:
+                color = "#80080ff";
+                break;
+        }
+
+        return string.Format("<color={0}>{1}</color>", color, title);
     }
 
     public void Remove()
